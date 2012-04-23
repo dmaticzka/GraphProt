83956b423a020ebeeed185196395d2419f231a99
Daniel Maticzka <titzke@gmail.com>
2012-04-23 14:55:10 +0200

    remove old shrep gspan code

--- create_shrep/tmp.sh
+++
@@ -1,2 +0,0 @@
-perl RNAtools/fasta2shrep_gspan_T.pl -fa PTB_pos_pad25.target100.fa -t 3 -M 5 -wins 50 -shift 10 | gzip > PTB_pos_pad25.target100.shrep_t3_T005_M5.gspan.gz
-perl RNAtools/fasta2shrep_gspan_T.pl -fa PTB_pos_pad25.target100.fa -t 3 -M 5 -wins 50 -shift 10 -cue | gzip > PTB_pos_pad25.target100.shrep_t3_T005_M5_cue.gspan.gz
