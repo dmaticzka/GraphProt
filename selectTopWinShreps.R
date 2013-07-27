c42f0d81f5b36ede585af9dcc8042024a84a0897
Daniel Maticzka <titzke@gmail.com>
2013-07-27 14:10:53 +0200

    select first position instead of last

--- selectTopWinShreps.R
+++ selectTopWinShreps.R
@@ -6,6 +6,6 @@ d <- read.table(nt_margins, col.names=c('shrep','position','score'))
 # get best scored windows per shrep
 d_top_wins <- ddply(d, .(shrep), function(df) {subset(df, score==max(score))})
 # get best scored womdow farthest to the right per shrep
-d_top_win <- ddply(d_top_wins, .(shrep), function(df) {subset(df, position==max(position))})
+d_top_win <- ddply(d_top_wins, .(shrep), function(df) {subset(df, position==min(position))})
 # write selection to stdout
 write.table(d_top_win, '', quote=F, col.names=F, row.names=F, sep="\t")
