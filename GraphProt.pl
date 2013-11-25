52607a78c6b4a6939ba8283402373b4a6b23a94e
Daniel Maticzka <titzke@gmail.com>
2013-11-25 22:25:37 +0100

    implement parameter checks

--- GraphProt.pl
+++ GraphProt.pl
@@ -32,6 +32,10 @@ Options:
     -affinities list of affinities
                     one value per line, same order as binding sites (fasta)
     -negfasta   fasta file containing negative class sequences
+    -R          GraphProt radius
+    -D          GraphProt distance
+    -bitsize    GraphProt bitsize
+    
     -debug      enable debug output
     -help       brief help message
     -man        full documentation
@@ -72,8 +76,8 @@ my $onlyseq;
 my $fasta;
 my $negfasta;
 my $affys;
-my $radius;
-my $distance;
+my $R;
+my $D;
 my $c;
 my $epsilon;
 my $epochs;
@@ -89,8 +93,8 @@ my $result = GetOptions (	"mode=s" => \$mode,
                             "fasta=s" => \$fasta,
                             "negfasta=s" => \$negfasta,
                             "affinities=s" => \$affys,
-                            "R=i" => \$radius,
-                            "D=i" => \$distance,
+                            "R=i" => \$R,
+                            "D=i" => \$D,
                             "c=f" => \$c,
                             "epsilon=f" => \$epsilon,
                             "epochs=i" => \$epochs,
@@ -113,7 +117,7 @@ pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
 my $quit_with_help = 0;
 
-sub check_fasta {
+sub check_param_fasta {
     if (not defined $fasta) {
         say STDERR "missing parameter: please specify a set of binding sites (fasta)";
         $quit_with_help = 1;
@@ -126,7 +130,7 @@ sub check_fasta {
     }
 }
 
-sub check_negfasta {
+sub check_param_negfasta {
     if (not defined $negfasta) {
         say STDERR "missing parameter: please specify a set of unbound sites";
         $quit_with_help = 1;
@@ -139,7 +143,7 @@ sub check_negfasta {
     }
 }
 
-sub check_affys {
+sub check_param_affys {
     if (not defined $affys) {
         say STDERR "missing parameter: please specify the list of affinities (affinities)";
         $quit_with_help = 1;
@@ -152,41 +156,147 @@ sub check_affys {
     }
 }
 
-sub check_params_regression {
-    $onlyseq or check_R
-    $onlyseq or check_D
-    $onlyseq or check_abstraction
-    check_bitsize
-    check_c
-    check_epsilon
+sub check_param_R {
+    if (not defined $R) {
+        say STDERR "missing parameter: please specify the radius (R)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($radius < 0) {
+        pod2usage("error: please specify a positive radius (R)");
+        $quit_with_help = 1;
+        return 0;
+    }
 }
 
-sub check_params_classification {
-    $onlyseq or check_R
-    $onlyseq or check_D
-    $onlyseq or check_abstraction
-    check_bitsize
-    check_epochs
-    check_epsilon
+sub check_param_D {
+    if (not defined $D) {
+        say STDERR "missing parameter: please specify the distance (D)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($D < 0) {
+        pod2usage("error: please specify a positive distance (D)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_bitsize {
+    if (not defined $bitsize) {
+        say STDERR "missing parameter: please specify the bitsize (bitsize)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($bitsize < 8) {
+        pod2usage("error: please specify a positive bitsize larger than 8 (bitsize)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_abstraction {
+    if (not defined $abstraction) {
+        say STDERR "missing parameter: please specify the RNAshapes abstraction level (abstraction)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($abstraction < 1 or $abstraction > 5) {
+        pod2usage("error: please specify an RNAshapes abstraction level between 1 and 5 (abstraction)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_c {
+    if (not defined $c) {
+        say STDERR "missing parameter: please specify SVR parameter c (c)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($c <= 0) {
+        pod2usage("error: please specify the SVR parameter c (c)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_epsilon {
+    if (not defined $epsilon) {
+        say STDERR "missing parameter: please specify SVR parameter epsilon (epsilon)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($c <= 0) {
+        pod2usage("error: please specify a positive epsilon (epsilon)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_epochs {
+    if (not defined $epochs) {
+        say STDERR "missing parameter: please specify SGD parameter epochs (epochs)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($epochs <= 0) {
+        pod2usage("error: please specify a value larger 0 (epochs)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_lambda {
+if (not defined $lambda) {
+        say STDERR "missing parameter: please specify SGD parameter lambda (lambda)";
+        $quit_with_help = 1;
+        return 0;
+    }
+    if ($lambda <= 0) {
+        pod2usage("error: please specify a positive lambda (lambda)");
+        $quit_with_help = 1;
+        return 0;
+    }
+}
+
+sub check_param_params_regression {
+    # only check when creating a structure model
+    $onlyseq or check_param_R
+    $onlyseq or check_param_D
+    $onlyseq or check_param_abstraction
+    check_param_bitsize
+    check_param_c
+    check_param_epsilon
+}
+
+sub check_param_params_classification {
+    # only check when creating a structure model
+    $onlyseq or check_param_R
+    $onlyseq or check_param_D
+    $onlyseq or check_param_abstraction
+    check_param_bitsize
+    check_param_epochs
+    check_param_lambda
 }
 
 defined $action or pod2usage("please specify the GraphProt action\n");
 
 if ($mode eq 'regression') {
     if ($action eq 'ls') {
-        check_fasta;
-        check_affys;
+        check_param_fasta;
+        check_param_affys;
     } elsif ($action eq 'cv') {
-        check_fasta;
-        check_affys;
-        check_params_regression;
+        check_param_fasta;
+        check_param_affys;
+        check_param_params_regression;
     } elsif ($action eq 'train') {
-        check_fasta;
-        check_affys;
-        check_params_regression;
+        check_param_fasta;
+        check_param_affys;
+        check_param_params_regression;
     } elsif ($action eq 'predict') {
-        check_fasta;
-        check_params_regression;
+        check_param_fasta;
+        check_param_params_regression;
     } elsif ($action eq 'predict_nt') {
         say STDERR "sorry, invalid action in regression setting";
         exit 2;
@@ -198,26 +308,26 @@ if ($mode eq 'regression') {
     }
 } elsif ($mode eq 'classification') {
     if ($action eq 'ls') {
-        check_fasta;
-        check_negfasta;
+        check_param_fasta;
+        check_param_negfasta;
     } elsif ($action eq 'cv') {
-        check_fasta;
-        check_negfasta;
-        check_params_classification;
+        check_param_fasta;
+        check_param_negfasta;
+        check_param_params_classification;
     } elsif ($action eq 'train') {
-        check_fasta;
-        check_negfasta;
-        check_params_classification;
+        check_param_fasta;
+        check_param_negfasta;
+        check_param_params_classification;
     } elsif ($action eq 'predict') {
-        check_fasta;
-        check_negfasta;
-        check_params_classification;
+        check_param_fasta;
+        check_param_negfasta;
+        check_param_params_classification;
     } elsif ($action eq 'predict_nt') {
-        check_fasta;
-        check_params_classification;    
+        check_param_fasta;
+        check_param_params_classification;    
     } elsif ($action eq 'motif') {
-        check_fasta;
-        check_params_classification;    
+        check_param_fasta;
+        check_param_params_classification;    
     } else {
         pod2usage("error: unknown action '$action'\n");
     }
