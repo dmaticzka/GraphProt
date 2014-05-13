f2a8f6e04884d025e95b725843bf44a72f5f1493
Daniel Maticzka <titzke@gmail.com>
2014-05-13 13:53:39 +0200

    start running tests; dirty result files

---
+++ tests/graphprot_test_classification_misc.sh
@@ -0,0 +1,32 @@
+################################################################################
+### classification
+
+## this is how I created the GraphProt test data for classification
+#echo; echo; echo; echo prepare input data
+#head -n 20 data/testclip.train.positives.fa > testclip.train.positives.fa
+#head -n 20 data/testclip.train.negatives.fa > testclip.train.negatives.fa
+
+echo; echo; echo; echo classification, cv
+../GraphProt.pl -mode classification -action cv -fasta testclip.train.positives.fa  -negfasta testclip.train.negatives.fa -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_cv --keep-tmp
+
+echo; echo; echo; echo classification, train
+../GraphProt.pl -mode classification -action train -fasta testclip.train.positives.fa  -negfasta testclip.train.negatives.fa -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_train --keep-tmp
+
+echo; echo; echo; echo classification, predict
+../GraphProt.pl -mode classification -action predict -fasta testclip.train.positives.fa  -negfasta testclip.train.negatives.fa -model CL_train.model -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_predict --keep-tmp
+
+echo; echo; echo; echo classification, predict using only positives
+../GraphProt.pl -mode classification -action predict    -fasta testclip.train.positives.fa -model CL_train.model  -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_predict_onlypos --keep-tmp
+
+echo; echo; echo; echo classification nt margins
+../GraphProt.pl -mode classification -action predict_profile --onlyseq -fasta testclip.train.positives.fa -model CL_train.model -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_ntmargins --keep-tmp
+
+echo; echo; echo; echo classification nt margins
+../GraphProt.pl -mode classification -action predict_has --onlyseq -fasta testclip.train.positives.fa -model CL_train.model -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -percentile 55 -prefix CL_has --keep-tmp
+
+echo; echo; echo; echo classification motif
+../GraphProt.pl -mode classification -action motif -fasta testclip.train.positives.fa -model CL_train.model  -abstraction 1 -R 0 -D 0 -epochs 2 -lambda 0.1 -bitsize 10 -prefix CL_motif --keep-tmp
+
+echo; echo; echo; echo classification motif onlyseq
+../GraphProt.pl -mode classification -action motif --onlyseq -fasta testclip.train.positives.fa -model CL_train.model -R 1 -D 0 -bitsize 10 -prefix CL_onlyseq --keep-tmp
+
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,11 @@
-bash graphprot_test_misc.sh &> graphprot_test_misc.log &
-bash graphprot_test_regression_ls.sh &> graphprot_test_regression_ls.log &
-bash graphprot_test_classification_ls.sh &> graphprot_test_classification_ls.log
+bash graphprot_test_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_misc.log
+
+bash graphprot_test_classification_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_misc.log
+
+bash graphprot_test_regression_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_misc.log
+
+bash graphprot_test_regression_ls.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_ls.log
+
+bash graphprot_test_classification_ls.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_ls.log
+
+gunzip *.gz
