8593239b1ae5f579e0a4ec4ceaf41f1e4b852ed7
Daniel Maticzka <titzke@gmail.com>
2014-05-13 15:28:48 +0200

    proper test output rerouting

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,11 @@
-bash graphprot_test_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_misc.log
+bash graphprot_test_misc.sh 2>&1 | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_misc.log
 
-bash graphprot_test_classification_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_misc.log
+bash graphprot_test_classification_misc.sh 2>&1 | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_misc.log
 
-bash graphprot_test_regression_misc.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_misc.log
+bash graphprot_test_regression_misc.sh 2>&1 | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_misc.log
 
-bash graphprot_test_regression_ls.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_ls.log
+bash graphprot_test_regression_ls.sh 2>&1 | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_regression_ls.log
 
-#bash graphprot_test_classification_ls.sh | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_ls.log
+bash graphprot_test_classification_ls.sh 2>&1 | grep --line-buffered -v "^Elapsed time" | sed -e 's/tmp-\S\{6\}/tmp-XXXXX/g' &> graphprot_test_classification_ls.log
 
 gunzip -f *.gz
