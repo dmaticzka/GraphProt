312710da5fc247f5c89ea8653e191f9d542daf5b
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2017-09-28 14:57:17 +0200

    test linesearch

---
+++ tests/test_graphprot_classification_ls.py
@@ -0,0 +1,26 @@
+from scripttest import TestFileEnvironment
+from filecmp import cmp
+
+testdir = "tests/testenv_graphprot_classification_ls/"
+
+env = TestFileEnvironment(testdir)
+
+
+def test_classification_ls():
+    "Run parameter linesearch."
+    call = """../../GraphProt.pl -mode classification -action ls \
+              -fasta ../testclip.train.positives.fa \
+              -negfasta ../testclip.train.negatives.fa -prefix CL_ls --keep-tmp"""
+    env.run(call)
+    cmp("tests/CL_ls.params", testdir + "CL_ls.params")
+
+
+def test_classification_train_from_lsparam():
+    "Train a model using parameter file."
+    call = """../../GraphProt.pl -mode classification -action train \
+              -fasta ../testclip.train.positives.fa \
+              -negfasta ../testclip.train.negatives.fa
+              -params ../CL_ls.params \
+              -prefix CL_train_from_ls --keep-tmp"""
+    env.run(call)
+    cmp("tests/CL_train_from_ls.model", testdir + "CL_train_from_ls.model")
