f4054b13b596cfa0bf3557534bd0142c036e99e8
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2017-09-28 17:24:46 +0200

    remove old tests

--- tests/graphprot_test_classification_ls.sh
+++
@@ -1,5 +0,0 @@
-echo; echo; echo; echo classification, ls
-../GraphProt.pl -mode classification -action ls -fasta testclip.train.positives.fa -negfasta testclip.train.negatives.fa -prefix CL_ls --keep-tmp
-
-echo; echo; echo; echo train model using parameters file from linesearch
-../GraphProt.pl -mode classification -action train -fasta testclip.train.positives.fa -negfasta testclip.train.negatives.fa -params CL_ls.params -prefix CL_train_from_ls --keep-tmp
--- tests/graphprot_test_regression_ls.sh
+++
@@ -1,6 +0,0 @@
-echo; echo; echo; echo regression, ls
-export PATH=/home/maticzkd/src/libsvm-3.12/:$PATH
-../GraphProt.pl -mode regression -action ls -fasta test_data_full_A.train.fa -affinities test_data_full_A.train.affys -prefix REG_ls -abstraction 1 -R 0 -D 0 -epsilon 0.11 -c 11 -bitsize 10 --keep-tmp
-
-echo; echo; echo; echo train model using parameters file from linesearch
-../GraphProt.pl -mode regression -action train -fasta test_data_full_A.train.fa -affinities test_data_full_A.train.affys -params REG_ls.params -prefix REG_train_from_ls --keep-tmp
